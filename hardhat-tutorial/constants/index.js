1ac582b101633afeadbefff5542f1db74a25607c
Viozhu <nachog_man@icloud.com>
2023-05-09 04:18:30 +0900

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0xCC7d18dB675E34f5Fe3dBa2Dc3fD8bb2cDF75150'
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
---
+++ my-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/layout.js": [
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/globals.css"
+    ],
+    "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/page.js": [
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/Home.module.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/page": [
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/globals.css",
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/jorgeignaciogaray/Desktop/Random/web3Test/NFT-Collection/my-app/src/app/Home.module.css"
+    ]
+  }
+}
